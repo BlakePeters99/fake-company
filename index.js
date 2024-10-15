b97724d7defe3f3fe8857d097210489a78c41726
Blake Peters <blakerdpeters@gmail.com>
2024-10-15 15:16:40 -0700

    inital project setup and working button image

---
+++ index.js
@@ -0,0 +1,10 @@
+const buttonElement = document.querySelector('#button1');
+const listElement = document.querySelector('.sidebar');
+
+buttonElement.addEventListener('click', () => {
+    //const element = document.createElement('li');
+    //element.textContent = 'test';
+    console.log("it's alive!!");
+    //listElement.appendChild(element);
+    
+});
