83d8cc11d6105c8e0f756728bf56cad69a65830f
Blake Peters <blakerdpeters@gmail.com>
2024-10-15 18:59:42 -0700

    Completed slide transition in TA hours

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const buttonElement = document.querySelector('#button1');
-const listElement = document.getElementById('sidebar');
+const listElement = document.querySelector('.sidebar');
 
 buttonElement.addEventListener('click', () => {
     console.log("it's alive!!");
-    if (listElement.style.display === 'none') {
-        listElement.style.display = 'block'
-    }
-    else {
-        listElement.style.display = 'none'
-    }
+    listElement.classList.toggle('show');
 });
+
